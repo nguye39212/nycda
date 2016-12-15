609d7863c0dcf1e1bc30a8920aa18d6a9367e5c2
Jimmy Nguyen <jimmynguyen1@att.net>
2016-12-15 16:59:02 -0600

    home work redo

--- homework/week2_day1_js.js
+++ homework/week2_day1_js.js
@@ -1,23 +1,38 @@
-function add(num1,num1)
-	{return num1+num2}
-var a=(5+6)
+function add(num1, num2){
+	return num1+num2
+}
 
-function subtract(num1-num2)
-	{return num1-num2}
+var a = add(5, 6);
+
+function subtract(num1, num2){
+	return num1-num2
+}
+
+// use subtract functon
 var b=(12-2)
-var c=(a+b)
 
-function divide(num1/num2)
-	{return num1/num2}
+var c = add(a,b)
+
+function divide(num1, num2){
+	return num1/num2
+}
+
+// use divide method
 var y=(80/12)
 
-function multiply(num1*num2)
-	{return num1*num2}
-var x=(5*6)
-var z=(x*y)
+function multiply(num1, num2){
+	return num1*num2
+}
+
+// use multiply method
+var x= multiply (5*6)
+//use multiply method
+var z= multiply (x*y)
 
 console.log (c)
 console.log (z)
 
 alert('the varible c is equal to'c)
 alert('the varible z is equal to'z)
+//git add .
+// git push
